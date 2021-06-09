2d03e63c666d6d9ab750ae58e238fbd1216b2ce2
LongHoangg <hoangphilong050698@gmail.com>
2021-06-09 17:37:02 +0700

    + add project

---
+++ CyberPucc/Assets/Helicopter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2.0f;
+
+    [SerializeField]
+    private GameObject soldier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0.0f, 0.0f));
+    }
+}

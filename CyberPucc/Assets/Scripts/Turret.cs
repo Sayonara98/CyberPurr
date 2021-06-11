e6e8a16b6bbd7e394e0757adf1401d2aebf537c0
LongHoangg <hoangphilong050698@gmail.com>
2021-06-11 16:20:32 +0700

    + implement shooting single and automic with mouse left click and fire rate to prevent fast click speed

--- CyberPucc/Assets/Scripts/Turret.cs
+++ CyberPucc/Assets/Scripts/Turret.cs
@@ -7,7 +7,12 @@ public class Turret : MonoBehaviour
     [SerializeField]
     private GameObject turretPos;
     [SerializeField]
-    private GameObject bullet;
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private float fireRate = 0.5f;
+    [SerializeField]
+    private float fireSpeed = 8f;
+    private bool canShoot = true;
 
     private Camera cam;
     private Vector3 mousePos;
@@ -23,6 +28,11 @@ public class Turret : MonoBehaviour
     void Update()
     {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0) && canShoot)
+        {
+            StartCoroutine(Shoot());
+        }
     }
 
     private void FixedUpdate()
@@ -31,5 +41,16 @@ public class Turret : MonoBehaviour
         lookDir.y = (lookDir.y < 0) ? 0 : lookDir.y;
         angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         turretPos.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle - 90f);
+    }
+
+    private IEnumerator Shoot()
+    {
+        GameObject missile = Instantiate(bulletPrefab, turretPos.transform.position, turretPos.transform.rotation) as GameObject;
+        //missile.transform.Translate(0.0f, 0.4f, 0.0f);
+        Rigidbody2D bulletRb = missile.GetComponent<Rigidbody2D>();
+        bulletRb.AddForce(turretPos.transform.up * fireSpeed, ForceMode2D.Impulse);
+        canShoot = false;
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
     }
 }

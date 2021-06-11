3b7dd7f2b919ff47e59fc02a75d077d4b63ff89e
LongHoangg <hoangphilong050698@gmail.com>
2021-06-11 17:22:55 +0700

    + Dog run toward base with walk animation when reach the ground

--- CyberPucc/Assets/Scripts/Soldier.cs
+++ CyberPucc/Assets/Scripts/Soldier.cs
@@ -7,6 +7,8 @@ public class Soldier : MonoBehaviour
     [SerializeField]
     private float parachuteSpeed = 1.0f;
 
+    private bool onTheGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,27 @@ public class Soldier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(parachuteSpeed * Time.deltaTime * Vector3.down);
+        if (onTheGround)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Vector3 moveToPlayerDir = player.transform.position - transform.position;
+            transform.Translate(moveToPlayerDir.normalized * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(parachuteSpeed * Time.deltaTime * Vector3.down);
+        }    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            if (onTheGround == false)
+            {
+                onTheGround = true;
+                GetComponent<Animator>().SetBool("onTheGround", onTheGround);
+            }
+        }
     }
 }
